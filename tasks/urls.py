4f3424f9e2c882ede0ccf589f410fe1e3c6ebd4c
Danilo Andrade <dandrade0803@gmail.com>
2023-07-30 19:13:47 -0300

    Criando pagina teste para treinar configuração de rotas

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from tasks.views import index
+from . import views
 
 
 urlpatterns = [
-    path("", index)
+    path("", views.index, name='index'),
+    path("teste/", views.teste, name='teste')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'tasks/index.html')
+
+
+def teste(request):
+    return render(request, 'tasks/teste.html')
